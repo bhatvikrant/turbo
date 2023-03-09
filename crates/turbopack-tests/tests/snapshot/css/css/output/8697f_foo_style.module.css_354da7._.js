b182ba83a0bf5dede3cc6fda648c21f1bc6a4927
Alex Kirszenberg <alex.kirszenberg@vercel.com>
2023-03-09 17:54:00 +0100

    Update snapshots

--- crates/turbopack-tests/tests/snapshot/css/css/output/8697f_foo_style.module.css_354da7._.js
+++ crates/turbopack-tests/tests/snapshot/css/css/output/8697f_foo_style.module.css_354da7._.js
@@ -1,4 +1,6 @@
-(self.TURBOPACK = self.TURBOPACK || []).push(["output/8697f_foo_style.module.css_354da7._.js", {
+(self.TURBOPACK = self.TURBOPACK || []).push([
+"output/8697f_foo_style.module.css_354da7._.js",
+{
 
 "[project]/crates/turbopack-tests/tests/snapshot/css/css/input/node_modules/foo/style.module.css (css, css module)": (({ r: __turbopack_require__, x: __turbopack_external_require__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, c: __turbopack_cache__, l: __turbopack_load__, k: __turbopack_register_chunk_list__, j: __turbopack_cjs__, p: process, g: global, __dirname }) => (() => {
 
@@ -8,6 +10,5 @@ __turbopack_export_value__({
 
 })()),
 }]);
-
 
 //# sourceMappingURL=8697f_foo_style.module.css_354da7._.js.map
--- crates/turbopack-tests/tests/snapshot/imports/static-and-dynamic/output/79fb1_turbopack-tests_tests_snapshot_imports_static-and-dynamic_input_vercel.mjs._.js
+++ crates/turbopack-tests/tests/snapshot/imports/static-and-dynamic/output/79fb1_turbopack-tests_tests_snapshot_imports_static-and-dynamic_input_vercel.mjs._.js
@@ -1,4 +1,6 @@
-(self.TURBOPACK = self.TURBOPACK || []).push(["output/79fb1_turbopack-tests_tests_snapshot_imports_static-and-dynamic_input_vercel.mjs._.js", {
+(self.TURBOPACK = self.TURBOPACK || []).push([
+"output/79fb1_turbopack-tests_tests_snapshot_imports_static-and-dynamic_input_vercel.mjs._.js",
+{
 
 "[project]/crates/turbopack-tests/tests/snapshot/imports/static-and-dynamic/input/vercel.mjs (ecmascript)": (({ r: __turbopack_require__, x: __turbopack_external_require__, i: __turbopack_import__, s: __turbopack_esm__, v: __turbopack_export_value__, c: __turbopack_cache__, l: __turbopack_load__, k: __turbopack_register_chunk_list__, j: __turbopack_cjs__, p: process, g: global, __dirname }) => (() => {
 
@@ -9,6 +11,5 @@ const __TURBOPACK__default__export__ = "turbopack";
 
 })()),
 }]);
-
 
 //# sourceMappingURL=79fb1_turbopack-tests_tests_snapshot_imports_static-and-dynamic_input_vercel.mjs._.js.map
